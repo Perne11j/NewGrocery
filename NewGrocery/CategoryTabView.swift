5b065b3867f9e6f1eb055f87136b6306c0bcfd31
pj <pernellj504@gmail.com>
2023-07-11 23:53:17 -0400

    Asset Commit

--- NewGrocery/CategoryTabView.swift
+++ NewGrocery/CategoryTabView.swift
@@ -13,7 +13,6 @@ struct CategoryTabView: View {
             CategoryTView(photoImage: "Alcohol", title: "Alcohol")
             CategoryTView(photoImage: "ReadyMeals", title: "Ready Meals")
             CategoryTView(photoImage: "Beauty", title: "Beauty")
-            CategoryTView(photoImage: "Pickup", title: "Pickup")
             
                 
         }
