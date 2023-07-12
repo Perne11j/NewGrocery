f85ba946b0c61d888cf67b85b42cb4950209dd75
pj <pernellj504@gmail.com>
2023-07-11 23:14:03 -0400

    CategoryTabView Commit

---
+++ NewGrocery/CategoryTabView.swift
@@ -0,0 +1,43 @@
+//
+//  CatView.swift
+//  NewGrocery
+//
+//  Created by Pernell Jones on 7/11/23.
+//
+
+import SwiftUI
+
+struct CategoryTabView: View {
+    var body: some View {
+        LazyHStack {
+            CategoryTView(photoImage: "Alcohol", title: "Alcohol")
+            CategoryTView(photoImage: "ReadyMeals", title: "Ready Meals")
+            CategoryTView(photoImage: "Beauty", title: "Beauty")
+            CategoryTView(photoImage: "Pickup", title: "Pickup")
+            
+                
+        }
+    }
+}
+
+struct CategoryTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryTabView()
+    }
+}
+
+struct CategoryTView: View {
+    let photoImage: String
+    let title: String
+    var body: some View {
+        ZStack {
+            Image(photoImage)
+            Text(title)
+                .font(.system(size: 25))
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+        }
+        
+
+    }
+}
